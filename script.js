072e7c7d29495b3a890a18b5dbf16de18029a185
Tobias Bertera <tobiasbertera@gmail.com>
2023-09-04 18:24:01 -0300

    Add dialog to get the user inputs

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let btn = document.querySelector(".btn-add");
 console.log(btn)
 
+
+btn.addEventListener("click", addBook)
+
 const myLibrary = ["Lord of the rings: Fellowship of the ring", 
                     "Lord of the rings: The two towers",
                     "Lord of the rings: Return of the king",]
 
 
+function Book (name, author, pages, year, readIt) {
+
+    this.name = name,
+    this.author = author,
+    this.year = year,
+    this.pages = pages,
+    this.readIt = readIt
+}
 
 function addBook () {
-
+    console.log("ola k ase")
 }
