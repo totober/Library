a59dcf12520d5a0aae740bf97ab02fee01e87c54
Tobias Bertera <tobiasbertera@gmail.com>
2023-09-04 17:02:04 -0300

    Add Layout and css

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector(".btn-add");
+console.log(btn)
+
+const myLibrary = ["Lord of the rings: Fellowship of the ring", 
+                    "Lord of the rings: The two towers",
+                    "Lord of the rings: Return of the king",]
+
+
+
+function addBook () {
+
+}
