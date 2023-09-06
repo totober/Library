91441f7a0f5f0096fab7201b0a4abd6e5a086d27
Tobias Bertera <tobiasbertera@gmail.com>
2023-09-06 08:42:25 -0300

    Add book and card creator function

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let btn = document.querySelector(".btn-add");
-console.log(btn)
+let dialog = document.querySelector("dialog");
+let add = document.querySelector(".add-book");
+let cancel = document.querySelector(".cancel-book");
+let main = document.querySelector("main");
+
+let title = document.getElementById("title");
+let writer = document.getElementById("author");
+let date = document.getElementById("date");
+let page = document.getElementById("page");
+let read = document.getElementsByName("user_read")
 
 
-btn.addEventListener("click", addBook)
 
-const myLibrary = ["Lord of the rings: Fellowship of the ring", 
-                    "Lord of the rings: The two towers",
-                    "Lord of the rings: Return of the king",]
+btn.addEventListener("click", displayModal);
+cancel.addEventListener("click", closeModal);
+add.addEventListener("click", addBook);
+
+
+
+
+const myLibrary = [];
 
 
 function Book (name, author, pages, year, readIt) {
@@ -18,6 +31,88 @@ function Book (name, author, pages, year, readIt) {
     this.readIt = readIt
 }
 
-function addBook () {
-    console.log("ola k ase")
+function displayModal (e) {
+    dialog.showModal();
 }
+
+function closeModal (e) {
+    dialog.close();
+} 
+
+function addBook (e) {
+
+    e.preventDefault();
+
+    let name = title.value.trim().toLowerCase();
+    console.log(name)
+    let author = writer.value.trim().toLowerCase();
+    console.log(author)
+    let year = date.value.trim().toLowerCase();
+    console.log(year)
+    let pages = page.value.trim().toLowerCase();
+    console.log(pages)
+    let readIt = () => {
+        for (let i = 0; i < read.length; i++){
+           if(read[i].checked) {
+              return read[i].value
+           }
+        }
+    }
+    console.log(readIt())
+
+    let obj = new Book(name, author, pages, year, readIt())
+    console.log(obj)
+
+    myLibrary.push(obj);
+
+    createCard(name, author, pages, year);
+
+    dialog.close();
+
+} 
+
+function createCard (name, author, pages, year) {
+
+    let article = document.createElement("article");
+    let btnCard = document.createElement("button");
+    let btnToggle = document.createElement("button");
+    let title = document.createElement("h3");
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let p3 = document.createElement("p");
+
+    article.className = "card";
+    btnCard.className = "btn-card";
+    btnToggle.className = "btn-toggle";
+    btnCard.textContent = "X";
+    title.textContent = name;
+    p1.textContent = `Author: ${author}`;
+    p2.textContent =  `Published: ${year}`;
+    p3.textContent =  `Number of pages: ${pages}`; 
+
+    main.appendChild(article);
+    article.appendChild(title);
+    article.appendChild(btnCard);
+    article.appendChild(p1);
+    article.appendChild(p2);
+    article.appendChild(p3);
+    article.appendChild(btnToggle);
+
+
+}
+
+/* function readOrNot (e) {
+
+    e.preventDefault();
+
+    for (let val of read){
+        console.log(val.value)
+       if(val.checked) {
+          return val.value
+       }
+    }
+}
+     */
+
+
+
