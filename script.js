7ca20cb86a2d43f3a894d9dc432a5e444b41968c
Tobias Bertera <tobiasbertera@gmail.com>
2023-09-11 18:44:38 -0300

    Add readOrNot function and button

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ let cancel2 = document.querySelector(".cancel-book2");
 let main = document.querySelector("main");
 let deleteBook = document.querySelector(".btn-card");
 let deleteBookConfirm = document.querySelector(".delete-book-confirm");
+let btnToggle = document.querySelector(".btn-toggle");
 
 
 let title = document.getElementById("title");
 let writer = document.getElementById("author");
 let date = document.getElementById("date");
 let page = document.getElementById("page");
-let read = document.getElementsByName("user_read")
-
+let read = document.getElementsByName("user_read");
 
 
 btn.addEventListener("click", displayModal);
@@ -22,7 +22,7 @@ cancel.addEventListener("click", closeModal);
 cancel2.addEventListener("click", closeModal2);
 add.addEventListener("click", addBook);
 deleteBook.addEventListener("click", bookDelete);
-
+btnToggle.addEventListener("click", readOrNot);
 
 
 const myLibrary = [];
@@ -45,7 +45,7 @@ Book.prototype.deleteFromArray = function() {
 }
 
 // if i want the "this" to be te card created //
-//Book.prototype.createCard = createCard
+Book.prototype.createCard = createCard
 
 
 function displayModal (e) {
@@ -69,7 +69,7 @@ function bookDelete (e) {
 
     console.log(e.target.parentElement);
 
-    let titleOf = e.target.parentElement.querySelector("h3")
+    let titleOf = e.target.parentElement.querySelector("h3");
 
     for(i=0; i < myLibrary.length; i++){
         if(titleOf.textContent === myLibrary[i].name) {
@@ -92,7 +92,20 @@ function addBook (e) {
 
     e.preventDefault();
 
-    let name = title.value.trim().toUpperCase();
+    let nameOf = () => {
+       let name = title.value.trim().toUpperCase();
+       let counter = 0;
+
+        for(i=0; i < myLibrary.length; i++){ 
+            if(name === myLibrary[i].name){ 
+                counter ++
+                name = title.value.trim().toUpperCase() + " " + `${counter}`
+            }
+        } 
+       return name
+    }
+
+    let name = nameOf();
     //console.log(name)
     let author = writer.value.trim().toLowerCase();
     //console.log(author)
@@ -108,20 +121,29 @@ function addBook (e) {
         }
     }
 
+    let readItValue = readIt()
+    console.log(readIt())
+    console.log(readItValue)
+
     let obj = new Book(name, author, pages, year, readIt());
 
     myLibrary.push(obj);
 
-    createCard(name, author, pages, year);
+    //createCard(name, author, pages, year, readIt);
 
-    // obj. createCard(name, author, pages, year); // if i want the "this" to be the card created
+     obj.createCard(name, author, pages, year); // if i want the "this" to be the card 
+    
+    let inputs = document.querySelectorAll("input");
+    inputs.forEach(input => input.value = "" )
+    inputs.forEach(input => input.checked = false )
+
 
     dialog.close();
 
 } 
 
 
-function createCard (name, author, pages, year) {
+function createCard (name, author, pages, year, ) {
 
     console.log(this)
     
@@ -132,16 +154,21 @@ function createCard (name, author, pages, year) {
     let p1 = document.createElement("p");
     let p2 = document.createElement("p");
     let p3 = document.createElement("p");
+    let p4 = document.createElement("p");
 
     article.className = "card";
     btnCard.className = "btn-card";
     btnCard.addEventListener("click", bookDelete);
-    btnToggle.className = "btn-toggle";
+    btnToggle.classList.add("btn-toggle", "not-read")
+    btnToggle.addEventListener("click", readOrNot);
     btnCard.textContent = "X";
+    btnToggle.textContent = "Wishlist";
     title.textContent = name;
     p1.textContent = `Author: ${author}`;
     p2.textContent =  `Published: ${year}`;
     p3.textContent =  `Number of pages: ${pages}`; 
+    p4.className = "toggle-para";
+    p4.textContent = "toggle if you already read the book";
 
     main.appendChild(article);
     article.appendChild(title);
@@ -149,7 +176,33 @@ function createCard (name, author, pages, year) {
     article.appendChild(p1);
     article.appendChild(p2);
     article.appendChild(p3);
-    article.appendChild(btnToggle);
+    article.appendChild(btnToggle)
+    article.appendChild(p4);
+
+}
+
+
+function readOrNot (e) {
+
+    let para = e.target.parentElement.querySelector(".toggle-para");
+    console.log(para)
+
+    if(e.target.textContent === "Wishlist") {
+        e.target.textContent = "Read It"
+        //e.target.classList.toggle("read")
+        e.target.classList.remove("not-read")
+        e.target.classList.add("read")
+        para.textContent = "toggle if you didn`t read the book yet"
+
+    } else {
+        e.target.textContent = "Wishlist"
+       // e.target.classList.toggle("not-read")
+       e.target.classList.add("not-read")
+       e.target.classList.remove("read")
+       para.textContent = "toggle if you already read the book"
+    }
+
+
 
 }
 
